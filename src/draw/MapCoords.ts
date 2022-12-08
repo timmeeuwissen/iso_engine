45b6863e6dce1cabc6ccf243182957fa046a51d3
Tim Meeuwissen <timmeeuwissen@gmail.com>
2022-12-08 10:13:07 +0100

    Water works better, not perfect

--- src/draw/MapCoords.ts
+++ src/draw/MapCoords.ts
@@ -1,19 +1,23 @@
 export type tMapCoords = {[key: number]: {[key: number]: tTileCoords}}
 export type tCoord = [number,number] 
 
+export enum eCoordLocations {
+    bottom,
+    right,
+    top,
+    left,
+}
+
+export const opposites: {[key in keyof typeof eCoordLocations]: eCoordLocations} = {
+    bottom: eCoordLocations.top,
+    right: eCoordLocations.left,
+    top: eCoordLocations.bottom,
+    left: eCoordLocations.right,
+}
+
 export type tTileCoords = {
-    unslanted: {
-        bottom: tCoord, 
-        right: tCoord, 
-        top: tCoord, 
-        left: tCoord
-    },
-    slanted: {
-        bottom: tCoord, 
-        right: tCoord, 
-        top: tCoord, 
-        left: tCoord
-    }
+    unslanted: { -readonly [key in keyof typeof eCoordLocations]: tCoord },
+    slanted: { -readonly [key in keyof typeof eCoordLocations]: tCoord }
 }
 
 export const MapCoords = () => {
--- src/draw/terrain/Water.ts
+++ src/draw/terrain/Water.ts
@@ -1,51 +1,99 @@
 import { tDrawable } from "../../Draw";
 import { tConfigTerrain } from "../Terrain";
 import { Map as WorldMap, tMapAt } from '../../Map';
-import { tCoord } from "../MapCoords";
+import { eCoordLocations, opposites, tCoord, tTileCoords } from "../MapCoords";
+
+type tRelevantTile = { -readonly [key in keyof typeof eCoordLocations]?: tCoord }
 
 export const Water: tDrawable = (terrainConfig, map, mapCoords, ctx, terrain) => {    
     const draw = () => {
         const waterLevel = terrainConfig.level.water;
         let lakePoints: tCoord[] = [];
+        let relevantTiles: tRelevantTile[] = [];
 
         map.iterate((xLoc, zLoc, mapAt) => {
-            if (typeof mapAt?.level == "undefined" || mapAt.level > waterLevel) {
+            if (typeof mapAt?.level == "undefined" || mapAt.level > waterLevel || mapAt < waterLevel) {
                 return;
             }
+
             // check if any of the edges dips underwater
             const coords = mapCoords.getAll()[xLoc][zLoc],
-                relativePosition = Object.entries(coords.slanted).reduce((acc, [coordKey, value]) => {
-                    const unslantedCoord: tCoord = coords.unslanted[coordKey as 'bottom' | 'left' | 'right' | 'top'];
-                    if (xLoc==3) {
-                        console.log(xLoc, zLoc, coords)
-                    }
-                    if (value[1] != unslantedCoord[1] ) {
-                        acc.below.push(value);
-                        console.log(xLoc, zLoc, coordKey, 'slanted is bigger than unslanted')
+                relativePosition = Object.entries(coords.slanted).reduce((acc, [key, value]) => {
+                    const coordKey = key as keyof typeof eCoordLocations;
+                    const unslantedCoord: tCoord = coords.unslanted[coordKey];
+                    if (value[1] !=  unslantedCoord[1] ) {
+                        acc.below[coordKey] = value;
                     }
                     else if (value[0] == unslantedCoord[0] && value[1] == unslantedCoord[1]) {
-                        acc.waterline.push(value);
+                        acc.waterline[coordKey] = value;
                     }
                     return acc;
                 },
                 {
-                    waterline: [] as tCoord[],
-                    below: [] as tCoord[]
+                    waterline: {} as { -readonly [key in keyof typeof eCoordLocations]?: tCoord },
+                    below: {} as { -readonly [key in keyof typeof eCoordLocations]?: tCoord }
                 }
             );
             
-            if (relativePosition.waterline.length && relativePosition.below.length) {
-                lakePoints = [...lakePoints, ...relativePosition.waterline]
+            // when all edges dip under water, we don't need a line to indicate the shore.
+            // when all edges are still at the water threshold level, nothing sunk under water
+            // when some edges dipped under water, we need to draw a line that makes sense
+            if (Object.keys(relativePosition.waterline).length && Object.keys(relativePosition.below).length) {
+                relevantTiles.push(relativePosition.waterline);
             }
         });
-        const sortedLakepoints = lakePoints.sort((coordA, coordB) =>{
-            return Math.atan2(coordA[1], coordA[0]) > Math.atan2(coordB[1], coordB[0]) ? 1 : 0
-        })
 
+        let currentTile = relevantTiles.shift()
+        if (currentTile) {
+            lakePoints = [...lakePoints, ...Object.values(currentTile) as tCoord[]]
+            while (relevantTiles.length) {
+                // find the neighbour
+                const nextIndex = relevantTiles.findIndex((relevantTile) => {
+                    let trying = true;
+
+                    Object.entries(relevantTile as {[key: string]: tCoord}).every(([relLocation, relCoord]) => {
+                        Object.entries(currentTile as {[key: string]: tCoord}).every(([curLocation, curCoord]) => {
+                            if((curLocation != relLocation)){
+                                if (relCoord[0] == curCoord[0] &&
+                                    relCoord[1] == curCoord[1]) {
+                                        trying = false;
+                                        return false;
+                                    }
+                            }        
+                            return trying;                        
+                        })
+                        return trying;
+                    });
+
+                    if (!trying) {
+                        return true;
+                    } 
+                });
+
+                
+                // filter all the relevant points from the neighbour
+                lakePoints = Object.entries(relevantTiles[nextIndex] as {[key: string]: tCoord}).reduce((acc, [newLocation, newCoord]) => {
+                    Object.entries(currentTile as {[key: string]: tCoord}).forEach(([curLocation]) => {
+                        if (curLocation != newLocation){
+                            acc.push(newCoord);
+                        }
+                        return acc;
+                    });
+
+                    return lakePoints;
+                }, lakePoints);
+
+                // carry on from the neighbour onwards
+                currentTile = relevantTiles[nextIndex];
+
+                // delete the neighbour from the list to search
+                relevantTiles.splice(nextIndex,1);
+            } 
+        }
         
-        console.log('lakepoints', sortedLakepoints);
+        console.log('lakepoints', lakePoints);
 
-        terrain.draw_lines(sortedLakepoints)
+        terrain.draw_lines(lakePoints)
         ctx.globalAlpha = 0.5
         ctx.fillStyle = '#00f'
         ctx.fill()
